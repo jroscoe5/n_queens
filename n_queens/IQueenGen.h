8c162fd25bc4cc9041c23a7dc55f3d3dd973a3a4
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-12-04 12:06:42 -0800

    interface started

---
+++ n_queens/IQueenGen.h
@@ -0,0 +1,14 @@
+/**
+ *	@file	- IQueenGen.h
+ *	@author	- Jonathon Roscoe | Seattle University | CS4600 Fall 2018
+ *	@summary- Interface for a solution generator to the N-Queens problem
+**/
+
+#ifndef IQUEENGEN_H
+#define IQUEENGEN_H
+class IQueenGen
+{
+public:
+	
+};
+#endif
